8888cffd4d410865475687f7cbde8f21b35b484e
dockoder <docar.alho.blue@gmail.com>
2021-07-11 02:48:20 +0100

    ajax

--- src/main/java/pt/dockoder/maratonajsf/bean/estudante/EstudanteRegistrarBean.java
+++ src/main/java/pt/dockoder/maratonajsf/bean/estudante/EstudanteRegistrarBean.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import pt.dockoder.maratonajsf.model.Estudante;
 
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import javax.persistence.Id;
 import java.io.Serializable;
@@ -15,17 +16,43 @@ import static java.util.Arrays.asList;
 @Named
 @Data
 @NoArgsConstructor
+@ViewScoped //mantem o estado do objecto enquanto estiver no mesmo view
 public class EstudanteRegistrarBean implements Serializable {
     private Estudante estudante = new Estudante();
     private String[] nomes = {"John", "Karl", "Mashid"} ;
     private List<String> nomesList = asList("Mary", "Lola", "Monika");
     private Set<String> nomesUnicos = new HashSet<>(asList("Maputo", "Kim", "Ebulo"));
     private Map<String, String> nomesMapeados = new HashMap<>();
+    private Boolean mostrarNota;
 
     {
         nomesMapeados.put("Mary", "Crazy");
         nomesMapeados.put("Lola", "Princess");
         nomesMapeados.put("Monika", "Nerd");
+    }
+
+    public void executar(){
+        System.out.println("Fazendo busca ao BD");
+        System.out.println("A Processar os dados");
+        System.out.println("A exibir os dados");
+    }
+
+    public void executar(String param){
+        System.out.println("Fazendo busca ao BD com o parametro: " +  param);
+        System.out.println("A Processar os dados");
+        System.out.println("A exibir os dados");
+    }
+
+    public String processar(String param){
+        return "A processar o parâmetro: " + param;
+    }
+
+    public String goNextPage(){
+        return "index2?faces-redirect=true";
+    }
+
+    public void exibirNotas(boolean action){
+        this.mostrarNota = action;
     }
 
 }
