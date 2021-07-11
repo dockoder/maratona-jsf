c829afcc8c52deaecccb21f48838ac7a22b32498
dockoder <docar.alho.blue@gmail.com>
2021-07-11 01:05:17 +0100

    enum & array

--- src/main/java/pt/dockoder/maratonajsf/bean/estudante/EstudanteRegistrarBean.java
+++ src/main/java/pt/dockoder/maratonajsf/bean/estudante/EstudanteRegistrarBean.java
@@ -7,6 +7,9 @@ import pt.dockoder.maratonajsf.model.Estudante;
 import javax.inject.Named;
 import javax.persistence.Id;
 import java.io.Serializable;
+import java.util.*;
+
+import static java.util.Arrays.asList;
 
 // gerenciado pelo CDI
 @Named
@@ -15,5 +18,14 @@ import java.io.Serializable;
 public class EstudanteRegistrarBean implements Serializable {
     private Estudante estudante = new Estudante();
     private String[] nomes = {"John", "Karl", "Mashid"} ;
+    private List<String> nomesList = asList("Mary", "Lola", "Monika");
+    private Set<String> nomesUnicos = new HashSet<>(asList("Maputo", "Kim", "Ebulo"));
+    private Map<String, String> nomesMapeados = new HashMap<>();
+
+    {
+        nomesMapeados.put("Mary", "Crazy");
+        nomesMapeados.put("Lola", "Princess");
+        nomesMapeados.put("Monika", "Nerd");
+    }
 
 }
